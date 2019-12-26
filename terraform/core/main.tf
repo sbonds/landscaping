7b184a9e1aaa39fabd417e84590212eea5ec0ee7
Karim Vaes <Karim.Vaes@microsoft.com>
2019-12-26 18:50:19 +0000

    azuread fixes

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -14,7 +14,11 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 provider "azuread" {
-  tenant_id = var.tenant_id
+  version = "=1.38.0"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 ######################################################################### RESOURCES
