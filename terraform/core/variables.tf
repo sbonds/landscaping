6405f1c29d0837c214f238ff4bf0ac037bffc6ca
Karim Vaes <Karim.Vaes@microsoft.com>
2019-12-26 14:56:16 +0000

    Added stages + Starting more granular refactoring

---
+++ terraform/core/variables.tf
@@ -0,0 +1,29 @@
+variable "tenant_id" {
+  type        = string
+  description = "The AD subscription in which to provision resources"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "The Azure subscription in which to provision resources"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for all the resources provisioned"
+}
+
+variable "region" {
+  type        = string
+  description = "The Azure region in which to provision resources"
+}
+
+variable "domain" {
+  type        = string
+  description = "The domain to use when configuring DNS and Traffic Manager"
+}
+
+variable "subdomain" {
+  type        = string
+  description = "The subdomain to use when configuring DNS and Traffic Manager"
+}
