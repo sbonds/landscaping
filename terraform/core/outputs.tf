68d051ef630ec5a1ea603232b61ccf0fb73be3f0
Karim Vaes <Karim.Vaes@microsoft.com>
2019-12-26 20:22:52 +0000

    Removed aad outputs

--- terraform/core/outputs.tf
+++ terraform/core/outputs.tf
@@ -17,11 +17,3 @@ output "traffic_manager_profile_name" {
 output "traffic_manager_profile_id" {
   value = azurerm_traffic_manager_profile.traffic_manager_profile.id
 }
-
-output "application_aks_cluster_application_id" {
-  value = azuread_application.application_aks_cluster.application_id
-}
-
-output "random_password_application_aks_cluster_result" {
-  value = random_password.random_password_application_aks_cluster.result
-}
