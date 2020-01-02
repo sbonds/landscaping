3a9f0000467da7a6d00b8851e7808a95fd10a161
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-02 15:55:01 +0000

    Fixed envs

--- scripts/aks-configure-acr-integration.sh
+++ scripts/aks-configure-acr-integration.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-echo $6
-echo $7
-az login -u $(ARMCLIENTID) -p $(ARMCLIENTSECRET)
-az account set -s $(armsubscriptionid)
+clientid=$1
+clientsecret=$2
+subscriptionid=$3
+module=$4
+environment=$5
+workload=$6
+registry=$7
+az login -u "$(clientid)" -p "$(clientsecret)"
+az account set -s "$(subscriptionid)"
 az extension add --name resource-graph
-az graph query -q "Resources" | where type =~ 'Microsoft.ContainerService/ManagedClusters' | where tags['Environment'] =~ "$(parameters.stage)" | where tags['Workload'] =~ "$(workload)" | project "name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c 'az aks update -g "$(parameters.stage)-$(workload)-$(parameters.module)" -n % --attach-acr "$(registry)";'
+az graph query -q "Resources" | where type =~ 'Microsoft.ContainerService/ManagedClusters' | where tags['Environment'] =~ "$(environment)" | where tags['Workload'] =~ "$(workload)" | project "name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$(environment)-$(workload)-$(module)\" -n % --attach-acr \"$(registry)\";"
+exit $?
