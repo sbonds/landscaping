eef91fbf7d7b8c39777129a1995dd71d8c12c51a
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-02 20:53:37 +0000

    fixed rg format in script

--- scripts/aks-configure-acr-integration.sh
+++ scripts/aks-configure-acr-integration.sh
@@ -12,5 +12,5 @@ tenantid=$8
 az login --service-principal -u "$clientid" -p "$clientsecret" --tenant "$tenantid"
 az account set -s "$subscriptionid"
 az extension add --name resource-graph
-az graph query -q "Resources | where type =~ \"Microsoft.ContainerService/ManagedClusters\" | where properties.provisioningState =~ \"Succeeded\" | where tags[\"Environment\"] =~ \"$environment\" | where tags[\"Workload\"] =~ \"$workload\" | project name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$environment-$workload-$module\" -n % --attach-acr \"$registry\";"
+az graph query -q "Resources | where type =~ \"Microsoft.ContainerService/ManagedClusters\" | where properties.provisioningState =~ \"Succeeded\" | where tags[\"Environment\"] =~ \"$environment\" | where tags[\"Workload\"] =~ \"$workload\" | project name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$workload-$environment-$module\" -n % --attach-acr \"$registry\";"
 exit $?
