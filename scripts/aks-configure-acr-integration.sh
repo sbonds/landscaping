ca1e4ae3fab57cea8ff2ddb02e0f909c5de2dcf9
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-02 17:01:25 +0000

    graph query fix

--- scripts/aks-configure-acr-integration.sh
+++ scripts/aks-configure-acr-integration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 clientid=$1
 clientsecret=$2
 subscriptionid=$3
@@ -6,8 +7,10 @@ module=$4
 environment=$5
 workload=$6
 registry=$7
-az login -u "$clientid" -p "$clientsecret"
+tenantid=$8
+
+az login --service-principal -u "$clientid" -p "$clientsecret" --tenant "$tenantid"
 az account set -s "$subscriptionid"
 az extension add --name resource-graph
-az graph query -q "Resources" | where type =~ 'Microsoft.ContainerService/ManagedClusters' | where tags['Environment'] =~ "$environment" | where tags['Workload'] =~ "$workload" | project "name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$environment-$workload-$module\" -n % --attach-acr \"$registry\";"
+az graph query -q "Resources | where type =~ \"Microsoft.ContainerService/ManagedClusters\" | where tags[\"Environment\"] =~ \"$environment\" | where tags[\"Workload\"] =~ \"$workload\" | project name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$environment-$workload-$module\" -n % --attach-acr \"$registry\";"
 exit $?
