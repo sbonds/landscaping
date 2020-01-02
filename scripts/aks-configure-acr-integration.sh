8e739b6be1ad4e497450ccebbc4bdc29c06678a5
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-02 16:12:06 +0000

    vars vars vars

--- scripts/aks-configure-acr-integration.sh
+++ scripts/aks-configure-acr-integration.sh
@@ -6,8 +6,8 @@ module=$4
 environment=$5
 workload=$6
 registry=$7
-az login -u "$(clientid)" -p "$(clientsecret)"
-az account set -s "$(subscriptionid)"
+az login -u "$clientid" -p "$clientsecret"
+az account set -s "$subscriptionid"
 az extension add --name resource-graph
-az graph query -q "Resources" | where type =~ 'Microsoft.ContainerService/ManagedClusters' | where tags['Environment'] =~ "$(environment)" | where tags['Workload'] =~ "$(workload)" | project "name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$(environment)-$(workload)-$(module)\" -n % --attach-acr \"$(registry)\";"
+az graph query -q "Resources" | where type =~ 'Microsoft.ContainerService/ManagedClusters' | where tags['Environment'] =~ "$environment" | where tags['Workload'] =~ "$workload" | project "name" -o yaml | awk '{ print $3 }' | xargs -I % sh -c "az aks update -g \"$environment-$workload-$module\" -n % --attach-acr \"$registry\";"
 exit $?
