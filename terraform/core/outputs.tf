2e16a7ac80a0acb35b96ea4866075aea05bb6f07
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-16 20:43:20 +0000

    outputs fix for tm

--- terraform/core/outputs.tf
+++ terraform/core/outputs.tf
@@ -10,10 +10,18 @@ output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.log_analytics_workspace.id
 }
 
-output "traffic_manager_profile_name" {
-  value = azurerm_traffic_manager_profile.traffic_manager_profile.name
+output "traffic_manager_profile_name_be" {
+  value = azurerm_traffic_manager_profile_be.traffic_manager_profile.name
 }
 
-output "traffic_manager_profile_id" {
-  value = azurerm_traffic_manager_profile.traffic_manager_profile.id
+output "traffic_manager_profile_id_be" {
+  value = azurerm_traffic_manager_profile_be.traffic_manager_profile.id
+}
+
+output "traffic_manager_profile_name_fe" {
+  value = azurerm_traffic_manager_profile_fe.traffic_manager_profile.name
+}
+
+output "traffic_manager_profile_id_fe" {
+  value = azurerm_traffic_manager_profile_fe.traffic_manager_profile.id
 }
