881640056ec0630f341ace62f7a59525d9beccd6
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-26 15:45:05 +0000

    Fixed finding istio gateway

--- scripts/aks-install-istio.sh
+++ scripts/aks-install-istio.sh
@@ -32,11 +32,11 @@ function getTrafficManagerResourcegroup {
 }
 
 function addIstioGatewayToTrafficManager {
-  servicename=`kubectl get services | grep -i "istio-ingressgateway" | awk '{ print $1 }'`
+  servicename="istio-ingressgateway"
   external_ip=""
   while [ -z $external_ip ]; do
     echo "Waiting for end point..."
-    external_ip=$(kubectl get svc "$servicename" --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+    external_ip=$(kubectl get svc "$servicename" --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}} --namespace istio-system")
     [ -z "$external_ip" ] && sleep 10
   done
   echo 'End point ready:' && echo $external_ip
