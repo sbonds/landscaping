1104a2e31dfeff19c99d52681abc0dfeebf5d091
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-26 15:09:49 +0000

    Add istio to TM and enable istio-injection

--- scripts/aks-install-istio.sh
+++ scripts/aks-install-istio.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 workload=$1
 environment=$2
+namespace=$3
 az extension add --name resource-graph
 
 # functions
@@ -19,12 +20,43 @@ function getClusterKubectl {
   echo $temp
 }
 
+function getTrafficManager {
+  component=$1
+  temp=`az graph query -q "Resources | where type =~ \"Microsoft.Network/trafficManagerProfiles\" | where tags[\"Component\"] =~ \"$component\" | where tags[\"Environment\"] =~ \"$environment\" | where tags[\"Workload\"] =~ \"$workload\" | project name" -o yaml | awk '{ print $3 }'`
+  echo $temp
+}
+
+function getTrafficManagerResourcegroup {
+  temp=`az graph query -q "Resources | where type =~ \"Microsoft.Network/trafficManagerProfiles\" | where name =~ \"$1\" | project resourceGroup" -o yaml | awk '{ print $3 }'`
+  echo $temp
+}
+
+function addIstioGatewayToTrafficManager {
+  servicename=`kubectl get services | grep -i "istio-ingressgateway" | awk '{ print $1 }'`
+  external_ip=""
+  while [ -z $external_ip ]; do
+    echo "Waiting for end point..."
+    external_ip=$(kubectl get svc "$servicename" --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+    [ -z "$external_ip" ] && sleep 10
+  done
+  echo 'End point ready:' && echo $external_ip
+  clustername=$1
+  component=$2
+  tmname=$(getTrafficManager $component)
+  tmrg=$(getTrafficManagerResourcegroup $tmname)
+  weight=1
+  az network traffic-manager endpoint create --resource-group "$tmrg" --profile-name "$tmname" --type externalEndpoints --name "$clustername" --target "$external_ip" --weight $weight
+}
+
 function deployToCluster {
   echo "Deploying to $1"
   clustername=$1
   clusterrg=$(getClusterResourcegroup $clustername)
   getClusterKubectl $clustername $clusterrg
   istioctl manifest apply --skip-confirmation
+  kubectl label namespace $namespace istio-injection=enabled
+  addIstioGatewayToTrafficManager $clustername "api"
+  addIstioGatewayToTrafficManager $clustername "www"
 }
 
 # main runtime
