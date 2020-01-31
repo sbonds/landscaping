bd6bd9700f62ac5a9c99f8ff6b10f9104afe443e
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-31 21:57:21 +0000

    Upgraded to 1.40 due to issue with aks

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -7,7 +7,7 @@ terraform {
 
 # Provider
 provider "azurerm" { 
-  version = "=1.38.0"
+  version = "~> 1.40.0"
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
