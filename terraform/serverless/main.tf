765da42ae2ed386be5cb1e24fb74fd5c055b20ab
Karim Vaes <Karim.Vaes@microsoft.com>
2020-02-13 20:25:48 +0000

    Added custom domain names to backend

--- terraform/serverless/main.tf
+++ terraform/serverless/main.tf
@@ -177,6 +177,24 @@ resource "azurerm_function_app" "functionapp" {
   }
 }
 
+resource "azurerm_app_service_custom_hostname_binding" "APIHOSTNAME" {
+  hostname            = "${data.terraform_remote_state.remote_state_core.outputs.APIHOSTNAME}"
+  app_service_name    = azurerm_app_service.functionapp.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "APIHOSTNAMEEXT" {
+  hostname            = "${data.terraform_remote_state.remote_state_core.outputs.APIHOSTNAMEEXT}"
+  app_service_name    = azurerm_app_service.functionapp.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "APIHOSTNAMEROOT" {
+  hostname            = "${data.terraform_remote_state.remote_state_core.outputs.APIHOSTNAMEROOT}"
+  app_service_name    = azurerm_app_service.functionapp.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
 resource "azurerm_traffic_manager_endpoint" "funcbe" {
   name                = "${var.workload}-${var.prefix}-func-be"
   resource_group_name = "${data.terraform_remote_state.remote_state_core.outputs.resource_group_name}"
